8b084b4e16d2e8bb71869d02394db85707021d26
Benjamin Mampaey <benjamin.mampaey@oma.be>
2019-02-12 13:29:38 +0000

    added time to start_date and end_date arguments of AIA_CH_pipeline.py

--- scripts/AIA_CH_pipeline.py
+++ scripts/AIA_CH_pipeline.py
@@ -7,6 +7,20 @@ from AIA_CH_spoca_jobs import run_spoca_jobs
 from AIA_CH_submit_events import submit_CHMap_events
 from event_db import EventDB
 
+def parse_date(date_string):
+	# TODO replace with fromisoformat when migrating to python 3.7
+	for date_format in '%Y-%m-%d', '%Y-%m-%dT%H', '%Y-%m-%dT%H:%M', '%Y-%m-%dT%H:%M:%S':
+		try:
+			result = datetime.strptime(date_string, date_format)
+		except ValueError:
+			pass
+		else:
+			break
+	else:
+		raise ValueError('Could not parse date string to datetime')
+	
+	return result
+
 # Start point of the script
 if __name__ == '__main__':
 	
@@ -14,8 +28,8 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description = 'Run the pipeline to create CH maps, track them, extract the events from them, and submit those events')
 	parser.add_argument('--debug', '-d', default = False, action = 'store_true', help = 'Set the logging level to debug')
 	parser.add_argument('--log_file', '-l', help = 'The file path of the log file')
-	parser.add_argument('--start_date', '-s', default = '2010-05-20', help = 'Start date of AIA files, in form YYYY-MM-DD')
-	parser.add_argument('--end_date', '-e', default = datetime.utcnow().strftime('%Y-%m-%d'), help = 'End date of AIA files, in form YYYY-MM-DD')
+	parser.add_argument('--start_date', '-s', default = '2010-05-20', type=parse_date, help = 'Start date of AIA files, in form YYYY-MM-DD')
+	parser.add_argument('--end_date', '-e', default = datetime.utcnow().strftime('%Y-%m-%d'), type=parse_date, help = 'End date of AIA files, in form YYYY-MM-DD')
 	parser.add_argument('--max_delay', '-m', default = 15, type = int, help = 'Max delay in days after which missing AIA files will be ignored')
 	parser.add_argument('--do_not_submit', action='store_true', help = 'If set, the events will not be submitted')
 	parser.add_argument('--untracked_maps', '-u', metavar = 'MAP', nargs='*', help = 'File paths of not yet tracked CH maps')
@@ -28,14 +42,9 @@ if __name__ == '__main__':
 	else:
 		logging.basicConfig(level = logging.DEBUG if args.debug else logging.INFO, format='%(asctime)s : %(levelname)-8s : %(message)s')
 	
-	# Parse the arguments
-	start_date = datetime.strptime(args.start_date, '%Y-%m-%d')
-	end_date = datetime.strptime(args.end_date, '%Y-%m-%d')
-	max_delay = timedelta(days=args.max_delay)
-	
 	# Run the spoca jobs
 	try:
-		CH_maps = run_spoca_jobs(start_date, end_date, max_delay, untracked_maps = args.untracked_maps)
+		CH_maps = run_spoca_jobs(args.start_date, args.end_date, max_delay = timedelta(days=args.max_delay), untracked_maps = args.untracked_maps)
 	except Exception as why:
 		logging.critical(str(why))
 		sys.exit(1)
