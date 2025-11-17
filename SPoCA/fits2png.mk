2e5dd79a258d1d989041bd2ce48520e5a05b98b5
Benjamin Mampaey <benjamin.mampaey@oma.be>
2025-11-17 12:04:03 +0000

    Updated requirements and SPoCA makefiles for newer librariries

--- SPoCA/fits2png.mk
+++ SPoCA/fits2png.mk
@@ -1,8 +1,8 @@
 CC=g++
 TRACKINGLFLAGS=-lpthread
 IDLLFLAGS=-L /usr/local/idl/idl706/bin/bin.linux.x86_64 -lpthread -lidl -lXp -lXpm -lXmu -lXext -lXt -lSM -lICE  -lXinerama -lX11 -ldl -ltermcap -lrt -lm /usr/lib/libXm.a
-MAGICKLFLAGS=`Magick++-config --ldflags --libs`
-MAGICKCFLAGS=`Magick++-config --cppflags`
+MAGICKLFLAGS=`pkg-config --libs Magick++-6.Q16`
+MAGICKCFLAGS=`pkg-config --cflags Magick++-6.Q16`
 CFLAGS=-Wall -fkeep-inline-functions -g -O3 $(MAGICKCFLAGS) 
 LFLAGS=-lcfitsio $(MAGICKLFLAGS) 
 DFLAGS= -DMAGICK 
